5e48fc04ab85337a46b6a1172aa7cfea85080a34
Boyce_Scout <49099741+ChrisBoyce886@users.noreply.github.com>
2019-08-03 14:32:19 -0400

    Adding Seeds

---
+++ models/seeds.sql
@@ -0,0 +1,6 @@
+SELECT * FROM fantasystats.qb;
+SELECT * FROM fantasystats.rb;
+SELECT * FROM fantasystats.wr;
+SELECT * FROM fantasystats.te;
+SELECT * FROM fantasystats.k;
+SELECT * FROM fantasystats.dst;
