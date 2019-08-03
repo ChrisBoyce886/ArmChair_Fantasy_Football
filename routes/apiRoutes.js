b39af4fc32696c4298c87c15c901c1c210e14557
mwmcguire <4903435649034356+mwmcguire@users.noreply.github.com>
2019-08-03 14:05:19 -0400

    updated server.js, models, and api-routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,29 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/players", function(req, res) {
+    db.Players.findAll({}).then(function(dbPlayers) {
+      res.json(dbPlayers);
+    });
+  });
+
+  app.get("/api/players/:position", function(req, res) {
+    db.Players.findAll({}).then(function(dbPlayers) {
+      res.json(dbPlayers);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/players", function(req, res) {
+    db.Players.create(req.body).then(function(dbPlayers) {
+      res.json(dbPlayers);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/players/:id", function(req, res) {
+    db.Players.destroy({ where: { id: req.params.id } }).then(function(dbPlayers) {
+      res.json(dbPlayers);
     });
   });
 };
