bc1e1339dd780fc1250c9addcb0b38bcd868df38
mwmcguire <4903435649034356+mwmcguire@users.noreply.github.com>
2019-08-07 23:38:37 -0400

    added socket.io chatroom

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,4 @@
 // Get references to page elements
-// var $exampleText = $("#example-text");
-// var $exampleDescription = $("#example-description");
-// var $submitBtn = $("#submit");
-// var $exampleList = $("#example-list");
-
 var $qbBtn = $("#qb-btn");
 var $rbBtn = $("#rb-btn");
 var $wrBtn = $("#wr-btn");
@@ -12,34 +7,7 @@ var $teBtn = $("#te-btn");
 var $kBtn = $("#k-btn");
 var $defBtn = $("#def-btn");
 
-// var $playerList = $("player-list");
-
-// The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function(example) {
-//     return $.ajax({
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       type: "POST",
-//       url: "api/examples",
-//       data: JSON.stringify(example)
-//     });
-//   },
-//   getTable: function(position) {
-//     return $.ajax({
-//       url: "players/table/" + position,
-//       type: "GET"
-//     });
-//   },
-//   deleteExample: function(id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE"
-//     });
-//   }
-// };
-
+// Get functions for getting player data and displaying to html
 var getQBs = function() {
   return $.ajax({
     url: "players/table/qb",
@@ -103,92 +71,7 @@ var getDEF = function() {
   });
 };
 
-// refreshExamples gets new examples from the db and repopulates the list
-// var refreshPlayers = function() {
-//   console.log("refreshing players");
-//   getTable().then(function(data) {
-//     $("#tableContainer").html(data);
-// for (var i = 0; i < data.length; i++) {
-//   var $tr = $("<tr>");
-//   var player = data[i];
-//   var records = ["noexist", "name", "team", "position", "comp", "yards", "touchdowns", "a", "b", "c"];
-
-//   records.forEach(function(record) {
-//     var value = player[record] || "";
-//     var $td = $("<td>").html(value);
-//     $tr.append($td);
-//   });
-
-//   $("tbody").append($tr);
-// }
-
-// var $td = $("<td>");
-
-// var $players = data.map(function() {
-//   var $a = $("<a>")
-//     .text(player.text)
-//     .attr("href", "/example/" + player.id);
-
-//   var $li = $("<li>")
-//     .attr({
-//       class: "list-group-item",
-//       "data-id": player.id
-//     })
-//     .append($a);
-
-//   var $button = $("<button>")
-//     .addClass("btn btn-danger float-right delete")
-//     .text("ｘ");
-
-//   $li.append($button);
-
-//   return $li;
-// });
-
-// $playerList.empty();
-// $playerList.append(result);
-//   });
-// };
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-// var handleFormSubmit = function(event) {
-//   event.preventDefault();
-
-//   var example = {
-//     text: $exampleText.val().trim(),
-//     description: $exampleDescription.val().trim()
-//   };
-
-//   if (!(example.text && example.description)) {
-//     alert("You must enter an example text and description!");
-//     return;
-//   }
-
-//   API.saveExample(example).then(function() {
-//     refreshExamples();
-//   });
-
-//   $exampleText.val("");
-//   $exampleDescription.val("");
-// };
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
-
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshExamples();
-//   });
-// };
-
-// Add event listeners to the submit and delete buttons
-// $submitBtn.on("click", handleFormSubmit);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
-
+// Event listeners for position buttons
 $qbBtn.on("click", getQBs);
 $rbBtn.on("click", getRBs);
 $wrBtn.on("click", getWRs);
