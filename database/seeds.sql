e25ecfd9a90f20ecc0a77d79e844f5ed2af7ee9d
Boyce_Scout <49099741+ChrisBoyce886@users.noreply.github.com>
2019-08-09 18:38:00 -0400

    html route change

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,6 +1,7 @@
 SELECT * FROM fantasystats.qbs;
 SELECT * FROM fantasystats.rbs;
 SELECT * FROM fantasystats.wrs;
+SELECT * FROM fantasystats.wr2s;
 SELECT * FROM fantasystats.tes;
 SELECT * FROM fantasystats.ks;
 SELECT * FROM fantasystats.dsts;
