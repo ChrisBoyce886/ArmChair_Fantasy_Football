4f1bc9e4ab8d4a0c74049cfb402c38da9fe6e282
Boyce_Scout <49099741+ChrisBoyce886@users.noreply.github.com>
2019-08-09 11:59:21 -0400

    seeds.sql

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,6 +1,7 @@
 SELECT * FROM fantasystats.qb;
 SELECT * FROM fantasystats.rb;
 SELECT * FROM fantasystats.wr;
+SELECT * FROM fantasystats.wr2;
 SELECT * FROM fantasystats.te;
 SELECT * FROM fantasystats.k;
 SELECT * FROM fantasystats.dst;
